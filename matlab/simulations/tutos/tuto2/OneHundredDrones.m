8a478339323c9b5414dc043178d8b2d2ed163f52
Rafael Casado <rafael.casado@uclm.es>
2024-01-07 17:59:53 +0100

    100 drones

--- matlab/simulations/tutos/tuto2/OneHundredDrones.m
+++ matlab/simulations/tutos/tuto2/OneHundredDrones.m
@@ -5,21 +5,18 @@ run('../../../tools/NAVSIM_PATHS');
 
 
 builder  = SimpleBuilder ("builder" ,NAVSIM_MODELS_PATH);
-operator = USpaceOperator("operator",NAVSIM_MODELS_PATH);
-
-max = 9;
-
-for i=0:max
-    for j = 0:max
+for i=0:9
+    for j = 0:9
         builder.DeployModel('DC/base_drone', ...
             ['BASE',num2str(i),num2str(j)], ...
             [i-4.5 j-4.5 0.26],[0 0 0]);
     end
 end
 
-
-for i=0:max
-    for j = 0:max
+%%
+operator = USpaceOperator("operator",NAVSIM_MODELS_PATH);
+for i=0:9
+    for j = 0:9
         operator.DeployUAV(                ...
             UAVmodels.MiniDroneCommanded,  ...
             ['UAV',num2str(i),num2str(j)], ...
@@ -27,10 +24,12 @@ for i=0:max
     end
 end
 
+
+%%
 pause(2)
 
-for i=0:max
-    for j = 0:max
+for i=0:9
+    for j = 0:9
         operator.RemoteCommand( ...
             ['UAV',num2str(i),num2str(j)], ...
             true,0,0,0.5,0,1);
@@ -39,8 +38,8 @@ end
 
 pause(10)
 
-for i=0:max
-    for j = 0:max
+for i=0:9
+    for j = 0:9
         operator.RemoteCommand( ...
             ['UAV',num2str(i),num2str(j)], ...
             true,2,0,0,1,100);
@@ -50,8 +49,8 @@ end
 pause(30)
 
 
-for i=0:max
-    for j = 0:max
+for i=0:9
+    for j = 0:9
         operator.RemoteCommand( ...
             ['UAV',num2str(i),num2str(j)], ...
             false,0,0,0,0,1);
