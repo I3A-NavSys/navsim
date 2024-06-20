37fad6686513c9d0776e2b5bed9f5d1418e63d53
Rafael Casado <rafael.casado@uclm.es>
2024-06-20 22:06:32 +0200

    clase Waypoint en python

--- ov/tmpRafa/operators/USpaceOperator.py
+++ ov/tmpRafa/operators/USpaceOperator.py
@@ -1,6 +1,3 @@
-#from omni.isaac.kit import SimulationApp
-#import carb
-
 class USpaceOperator:
 
     name = ""           # Operator name
@@ -10,21 +7,21 @@ class USpaceOperator:
     def __init__(self, name):
         self.name = name
         self.is_running = False
-        #self.sim_app = SimulationApp({"headless": False})  # Configura el entorno de la simulación
+
 
 
     def play_simulation(self):
         if not self.is_running:
             self.is_running = True
-          #  self.sim_app.update()  # Método para iniciar la simulación
             print(f"Simulation '{self.name}' started.")
         else:
             print(f"Simulation '{self.name}' is already running.")
 
+
+
     def pause_simulation(self):
         if self.is_running:
             self.is_running = False
-            #self.sim_app.pause()  # Método para pausar la simulación
             print(f"Simulation '{self.name}' paused.")
         else:
             print(f"Simulation '{self.name}' is not running.")
--- ov/tmpRafa/sims/sim01.py
+++ ov/tmpRafa/sims/sim01.py
@@ -1,8 +1,16 @@
+import sys
+import os
+
+# Añadir el directorio principal al sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+if os.name == 'nt':
+    os.system('cls')
+else:
+    os.system('clear')
+
 from operators.USpaceOperator import USpaceOperator
-import carb
-
-
-print("patata")
+from planners.FlightPlan import Waypoint
 
 # Crear una instancia de UspaceOperator
 op = USpaceOperator("Operador Uno")
@@ -10,4 +18,17 @@ op = USpaceOperator("Operador Uno")
 # Acceder al atributo name
 print(op.name)  # Imprime: Operador Uno
 
-op.play_simulation
+
+
+
+
+
+
+
+# Ejemplo de uso
+waypoint = Waypoint(label='WP1', t=10, pos=[1, 2, 3], vel=[0.1, 0.2, 0.3])
+print(waypoint.label)
+print(waypoint.t)
+print(waypoint.pos)
+print(waypoint.vel)
+print(waypoint.mandatory)
