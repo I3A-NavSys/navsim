2ba093f6f11166dec733a05857e56701df8727c2
BlazquezVictor <Victor.Blazquez@uclm.es>
2024-09-02 09:16:22 +0200

    new quadcopter added for joystick control

---
+++ ov/fleet/joystick_quadcopter/msgs/flight_plan_msg.py
@@ -0,0 +1,11 @@
+from .waypoint_msg import WaypointMsg
+
+class FlightPlanMsg:
+    def __init__(self) -> None:
+        self.plan_id: int
+        self.uav_id: str
+        self.operator_id: str
+        self.route: list[WaypointMsg] = []
+        self.mode: str
+        self.radius: float
+        self.priority: int
