e66f607745b587e1438dae8baf577f36ffa1ddac
BlazquezVictor <Victor.Blazquez@uclm.es>
2024-09-02 09:16:42 +0200

    joystick control added to new quadcopter

--- ov/extensions/external_control/external_control_python/ExternalController.py
+++ ov/extensions/external_control/external_control_python/ExternalController.py
@@ -4,6 +4,9 @@ from .KeyboardInput import KeyboardInput
 import numpy as np
 import asyncio
 
+import carb.events
+import omni.kit.app
+
 from pxr import UsdGeom, Gf, Usd
 
 class ExternalController:
@@ -22,6 +25,11 @@ class ExternalController:
         self.joystick = JoystickInput()
         self.keyboard = KeyboardInput()
 
+        # Create the event have a communication between the UAV and the joystick
+        self.CONTROL_JOYSTICK_EVENT = carb.events.type_from_string("omni.NavSim.ExternalControl.CONTROL_JOYSTICK_EVENT")
+        # Get the bus event stream
+        self.msg_bus_event_stream = omni.kit.app.get_app_interface().get_message_bus_event_stream()
+
     
     def start(self, prim, controller = None):
         if self._stop:
@@ -33,24 +41,8 @@ class ExternalController:
             # Selected drone
             self.prim = prim
 
-            # Attributes required
-            self._vel_att = self.prim.GetAttribute("physics:velocity")
-            self._ang_vel_att = self.prim.GetAttribute("physics:angularVelocity")
-            self._rb_gravity_att = self.prim.GetAttribute("physxRigidBody:disableGravity")
-
-            # New values for those attributes
-            self._vel_to_apply = Gf.Vec3f(0,0,0)
-            self._ang_vel_to_apply = Gf.Vec3f(0,0,0)
-
             # Needed variables
-            self.last_rotation = 0
-            self.last_z_neg_force = 0
             self.inputs = [0,0,0,0]
-
-            # Reset attributes
-            self._vel_att.Set(self._vel_to_apply)
-            self._ang_vel_att.Set(self._ang_vel_to_apply)
-            self.prim.GetAttribute("physxForce:force").Set(Gf.Vec3f(0,0,0))
             
             # Check if a specific controller must be used
             if self.controller != None:
@@ -88,20 +80,8 @@ class ExternalController:
                 self.joystick.stop()
                 self.keyboard.stop()
 
-            # TEST
-            self.prim.SetCustomData({"x_vel": 0, "y_vel": 0, "z_vel": 0, "z_rot": 0})
 
-            # New values for those attributes
-            # self._vel_to_apply = Gf.Vec3f(0,0,0)
-            # self._ang_vel_to_apply = Gf.Vec3f(0,0,0)
-
-            # # Reset attributes
-            # self._vel_att.Set(self._vel_to_apply)
-            # self._ang_vel_att.Set(self._ang_vel_to_apply)
-            # self.prim.GetAttribute("physxForce:force").Set(Gf.Vec3f(0,0,0))
-
-
-    # -- FUNCTION get_grav_deacc --------------------------------------------------------------------------------
+    # -- FUNCTION control --------------------------------------------------------------------------------
     # This method simply gets the inputs from both the joystick and the keyboard, then decide which one to use
     # Afterwards, it calls the corresponding methods to control the UAV
     # -----------------------------------------------------------------------------------------------------------
@@ -113,17 +93,10 @@ class ExternalController:
             vel = self.inputs[:3] * -self.linear_vel_limit
             rot = self.inputs[3] * -self.ang_vel_limit
 
-            # Update prim's custom data dictionary
-            self.prim.SetCustomData({"x_vel": vel[0], "y_vel": vel[1], "z_vel": vel[2], "z_rot": rot})
-
-
-            # Compute velocities
-            # self.compute_ang_vel()
-            # self.compute_lin_vel()
-
-            # # Update velocities
-            # self._vel_att.Set(self._vel_to_apply)
-            # self._ang_vel_att.Set(self._ang_vel_to_apply)
+            # Set inputs data structure
+            inputs = {"x_vel": vel[0], "y_vel": vel[1], "z_vel": vel[2], "z_rot": rot}
+            # Push CONTROL_JOYSTICK_EVENT with the inputs
+            self.msg_bus_event_stream.push(self.CONTROL_JOYSTICK_EVENT, payload={"method": "set_flight_inputs", "inputs": inputs})
 
             await asyncio.sleep(0.1)
 
@@ -166,189 +139,3 @@ class ExternalController:
                 return True
             
         return False
-    
-
-    # -- FUNCTION get_grav_deacc --------------------------------------------------------------------------------
-    # This method is used to simulate gravity when the default one is not working properly
-    # -----------------------------------------------------------------------------------------------------------
-    def get_grav_deacc(self, current_vel):
-        return current_vel - 9.8 * self._time_step
-    
-
-    # -- FUNCTION stop_inertia ----------------------------------------------------------------------------------
-    # This method just smooths the rotation of the drone
-    # -----------------------------------------------------------------------------------------------------------
-    def stop_inertia(self, last_rotate):
-        if last_rotate != 0:
-            if last_rotate > 0 and last_rotate < 8: return 0
-            if last_rotate < 0 and last_rotate > -8: return 0
-
-            if last_rotate > 0: return last_rotate - 8
-            if last_rotate < 0: return last_rotate + 8
-
-        return 0
-    
-    
-    # -- FUNCTION get_fordward_vector ---------------------------------------------------------------------------
-    # This method is in charge of computing the fordward vector of the drone, so that linear velocities are
-    # properly setted
-    # -----------------------------------------------------------------------------------------------------------
-    def get_fordward_vector(self, axis):
-        xformable = UsdGeom.Xformable(self.prim)
-        transform_matrix = xformable.ComputeLocalToWorldTransform(Usd.TimeCode.Default())
-
-        match axis:
-            # X is fordward
-            case 0:
-                fordward_vector = Gf.Vec3f(transform_matrix[0][0], transform_matrix[0][1], transform_matrix[0][2])                
-
-            # Y is fordward
-            case 1:
-                fordward_vector = Gf.Vec3f(transform_matrix[1][0], transform_matrix[1][1], transform_matrix[1][2])
-
-        return fordward_vector
-    
-
-    # -- FUNCTION get_orthogonal_vectors ------------------------------------------------------------------------
-    # This method is similar to get_fordward_vector
-    # It gets the orthogonal vectors to the fordward one, so that the drone can move among the three axis
-    # -----------------------------------------------------------------------------------------------------------
-    def get_orthogonal_vectors(self, forward_axis, fordward_vector):
-        match forward_axis:
-            # X is fordward
-            case 0:
-                up_vector = Gf.Vec3f(0,0,1)
-
-                left_vector = Gf.Cross(up_vector, fordward_vector)
-                up_vector = Gf.Cross(fordward_vector, left_vector)
-
-                return left_vector, up_vector
-            
-            # Y is fordward
-            case 1:
-                up_vector = Gf.Vec3f(0,0,1)
-
-                right_vector = Gf.Cross(fordward_vector, up_vector)
-                up_vector = Gf.Cross(right_vector, fordward_vector)
-
-                return right_vector, up_vector
-            
-
-    # -- FUNCTION compute_ang_vel -------------------------------------------------------------------------------
-    # This method is in charge of controlling the angular velocity, and hence the rotation of the UAV
-    # -----------------------------------------------------------------------------------------------------------
-    def compute_ang_vel(self):
-        rotation = self.inputs[3] * -self.ang_vel_limit
-
-        # If rotation is applied, accumulate it for a simulated inertia
-        if rotation != 0:
-            self.last_rotation += rotation
-            
-        # Simulate inertia
-        else:
-            self.last_rotation = self.stop_inertia(self.last_rotation)
-
-        # Update orienation
-        self._ang_vel_to_apply = Gf.Vec3f(0, 0, self.last_rotation)
-
-
-    # -- FUNCTION compute_lin_vel -------------------------------------------------------------------------------
-    # This method is similar to compute_ang_vel, but it controls the linear velocity
-    # -----------------------------------------------------------------------------------------------------------
-    def compute_lin_vel(self):
-        # Boolean smoothing variables
-        if self.inputs[0] == 0.0 and self.inputs[1] == 0.0: 
-            smooth_xy = True
-        else: 
-            smooth_xy = False
-
-
-        if self.inputs[2] >= 0.9: 
-            gravity = True
-            self.last_z_neg_force = self._vel_att.Get()[2]
-        else: 
-            gravity = False
-
-        # if joy_force[2] > 0: joy_force[2] = 0     # Avoid down force
-        inputs = self.inputs[:3] * -self.linear_vel_limit          # Revert axis and set maximum limit
-
-        # Get vector directions
-        fordward_vector = self.get_fordward_vector(0)
-        left_vector, up_vector = self.get_orthogonal_vectors(0, fordward_vector)
-
-        # Moving and going up
-        if not smooth_xy and not gravity:
-            # Disable gravity to avoid flickering
-            self._rb_gravity_att.Set(True)
-
-            x_dir_vel = fordward_vector * inputs[0]
-            y_dir_vel = left_vector * inputs[1]
-            z_dir_vel = up_vector * inputs[2]
-
-            if self.last_z_neg_force < inputs[2]:
-                self.last_z_neg_force += 0.8
-                z_dir_vel = Gf.Vec3f(0,0, self.last_z_neg_force)
-            
-            self._vel_to_apply = x_dir_vel + y_dir_vel + z_dir_vel
-
-        # Not moving
-        elif smooth_xy:
-            current_x_vel = self._vel_att.Get()[0]
-            current_y_vel = self._vel_att.Get()[1]
-            current_z_vel = self._vel_att.Get()[2]
-
-            # Smooth X
-            if current_x_vel >= 0 and current_x_vel < 0.5: current_x_vel = 0
-            elif current_x_vel <= 0 and current_x_vel > -0.5: current_x_vel = 0
-
-            if current_x_vel >= 0.5: current_x_vel -= 0.5
-            elif current_x_vel <= -0.5: current_x_vel += 0.5
-
-            # Smooth Y
-            if current_y_vel >= 0 and current_y_vel < 0.5: current_y_vel = 0
-            elif current_y_vel <= 0 and current_y_vel > -0.5: current_y_vel = 0
-
-            if current_y_vel >= 0.5: current_y_vel -= 0.5
-            elif current_y_vel <= -0.5: current_y_vel += 0.5
-
-            self._vel_to_apply = Gf.Vec3f(current_x_vel, current_y_vel, current_z_vel)
-
-            # Not moving and going down
-            if gravity:
-                # Uncomment this if nvidia gravity is not working properly, and comment the gravity attribute
-                # current_z_vel = self.get_grav_deacc(current_z_vel)
-                # self._vel_to_apply = Gf.Vec3f(current_x_vel, current_y_vel, current_z_vel)
-                
-                # Enable gravity
-                self._rb_gravity_att.Set(False)
-
-            # Not moving and going up
-            else:
-                # Disable gravity to avoid flickering
-                self._rb_gravity_att.Set(True)
-
-                if self.last_z_neg_force < inputs[2]:
-                    self.last_z_neg_force += 0.8
-                    self._vel_to_apply = Gf.Vec3f(current_x_vel, current_y_vel, self.last_z_neg_force)
-
-                else:
-                    z_dir_vel = up_vector * inputs[2]
-                    z_dir_vel[0] = current_x_vel
-                    z_dir_vel[1] = current_y_vel
-
-                    self._vel_to_apply = z_dir_vel
-
-        # Moving and going down
-        else:
-            # Uncomment this if nvidia gravity is not working properly, and comment the gravity attribute
-            # current_z_vel = self._vel_att.Get()[2]
-            # current_z_vel = self.get_grav_deacc(current_z_vel)
-
-            # Enable gravity
-            self._rb_gravity_att.Set(False)
-
-            x_dir_vel = fordward_vector * inputs[0]
-            y_dir_vel = left_vector * inputs[1]
-            z_dir_vel = Gf.Vec3f(0,0, self._vel_att.Get()[2])
-
-            self._vel_to_apply = x_dir_vel + y_dir_vel + z_dir_vel
