5e6e6ad7c138ba1166391e16f27301c9b55b23e5
Iker Nebot Amoriza <ikernebot@gmail.com>
2024-10-09 00:49:47 +0200

    core symlink added, still buggy

--- ov/extensions/navsim_core/navsim_core_python/extension.py
+++ ov/extensions/navsim_core/navsim_core_python/extension.py
@@ -1,4 +1,8 @@
 import omni.ext
+import os
+import platform
+
+from pathlib import Path
 
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
 # instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
@@ -8,6 +12,26 @@ class NavsimCore(omni.ext.IExt):
     # this extension is located on filesystem.
     def on_startup(self, ext_id):
         print("[navsim_core] navsim_core startup")
+
+        # Get current file path
+        src = Path(__file__)
+
+        # Move the source path five levels up to the root directory
+        for _ in range(5):
+            src = src.parent
+        
+        # Get destination file path
+        dst = Path(os.path.expandvars(r"%APPDATA%\..\Local\ov\pkg\isaac-sim-4.1.0"))
+
+        # Create the symlink if it doesn't already exist
+        if dst.exists():
+            try:
+                os.symlink(src.as_posix(), dst.as_posix(), target_is_directory=True)
+                print(f"Enlace simbólico creado: {src} -> {dst}")
+            except OSError as e:
+                print(f"Error al crear el enlace simbólico: {e}")
+        else:
+            print(f"El enlace simbólico ya existe: {dst}")
 
     def on_shutdown(self):
         print("[navsim_core] navsim_core shutdown")
