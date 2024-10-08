9f67c08a5b97151c988fe0e50b7b1af4ad41e857
Iker Nebot Amoriza <ikernebot@gmail.com>
2024-10-08 15:26:02 +0200

    core extension ui removed

--- ov/extensions/navsim_core/navsim_core_python/extension.py
+++ ov/extensions/navsim_core/navsim_core_python/extension.py
@@ -1,12 +1,4 @@
 import omni.ext
-import omni.ui as ui
-
-
-# Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
-def some_public_function(x: int):
-    print("[navsim_core] some_public_function was called with x: ", x)
-    return x ** x
-
 
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
 # instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
@@ -16,28 +8,6 @@ class NavsimCore(omni.ext.IExt):
     # this extension is located on filesystem.
     def on_startup(self, ext_id):
         print("[navsim_core] navsim_core startup")
-
-        self._count = 0
-
-        self._window = ui.Window("My Window", width=300, height=300)
-        with self._window.frame:
-            with ui.VStack():
-                label = ui.Label("")
-
-
-                def on_click():
-                    self._count += 1
-                    label.text = f"count: {self._count}"
-
-                def on_reset():
-                    self._count = 0
-                    label.text = "empty"
-
-                on_reset()
-
-                with ui.HStack():
-                    ui.Button("Add", clicked_fn=on_click)
-                    ui.Button("Reset", clicked_fn=on_reset)
 
     def on_shutdown(self):
         print("[navsim_core] navsim_core shutdown")
