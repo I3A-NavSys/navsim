e56e0c560c88f743731a32b61ff8797483766d75
BlazquezVictor <Victor.Blazquez@uclm.es>
2024-10-09 10:16:47 +0200

    navsim core ext update

--- ov/extensions/navsim_core/navsim_core_python/extension.py
+++ ov/extensions/navsim_core/navsim_core_python/extension.py
@@ -3,6 +3,7 @@ import os
 import platform
 
 from pathlib import Path
+import logging
 
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
 # instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
@@ -13,25 +14,32 @@ class NavsimCore(omni.ext.IExt):
     def on_startup(self, ext_id):
         print("[navsim_core] navsim_core startup")
 
+        logger = logging.getLogger("NavsimCore")
+
         # Get current file path
-        src = Path(__file__)
+        dst = Path(__file__)
 
         # Move the source path five levels up to the root directory
         for _ in range(5):
-            src = src.parent
+            dst = dst.parent
+
+        dst = dst.as_posix() + "/app"
         
         # Get destination file path
-        dst = Path(os.path.expandvars(r"%APPDATA%\..\Local\ov\pkg\isaac-sim-4.1.0"))
+        src = Path(os.path.expandvars(r"%APPDATA%\..\\Local\\ov\\pkg\\isaac-sim-4.1.0"))
 
         # Create the symlink if it doesn't already exist
-        if dst.exists():
+        if src.exists():
             try:
-                os.symlink(src.as_posix(), dst.as_posix(), target_is_directory=True)
-                print(f"Enlace simbólico creado: {src} -> {dst}")
+                os.symlink(src.as_posix(), dst, target_is_directory=True)
+                # print(f"Enlace simbólico creado: {src} -> {dst}")
+                logger.info((f"Enlace simbólico creado: {src} -> {dst}"))
             except OSError as e:
-                print(f"Error al crear el enlace simbólico: {e}")
+                # print(f"Error al crear el enlace simbólico: {e}")
+                logger.info(f"Error al crear el enlace simbólico: {e}")
         else:
-            print(f"El enlace simbólico ya existe: {dst}")
+            # print(f"El enlace simbólico ya existe: {dst}")
+            logger.info(f"El enlace simbólico ya existe: {dst}")
 
     def on_shutdown(self):
         print("[navsim_core] navsim_core shutdown")
