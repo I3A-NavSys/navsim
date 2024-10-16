863d2d43df8046da04952715a20c7d5b47a61d3d
BlazquezVictor <Victor.Blazquez@uclm.es>
2024-10-16 08:26:55 +0200

    uav race ext update

--- ov/extensions/uav_race/uav_race_python/extension.py
+++ ov/extensions/uav_race/uav_race_python/extension.py
@@ -1,43 +1,163 @@
+# Standard library imports
+import sys, os
+
+
+# Related third party imports
 import omni.ext
 import omni.ui as ui
+import omni.kit.viewport.window
+from omni.kit.widget.viewport import ViewportWidget
+
+project_root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+# Add root 'ov' folder to sys.path
+if project_root_path not in sys.path:
+    sys.path.append(project_root_path)
 
 
-# Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
-def some_public_function(x: int):
-    print("[uav_race] some_public_function was called with x: ", x)
-    return x ** x
+KIT_GREEN = 0xFF8A8777
+LABEL_PADDING = 120
 
 
-# Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
-# instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
-# on_shutdown() is called.
+Window_dark_style = {
+    "Window": {"background_color": 0xFF444444}
+}
+
+
+VStack_A = {
+    "VStack": {
+        "margin_width": 10, 
+        "margin_height": 0
+    }
+}
+
+
+VStack_B = {
+    "VStack": {"margin_width": 15}
+}
+
+
+Label_A = {
+    "Label": {
+        "font_size": 12,
+        "color": 0xFFDDDDDD
+    }
+}
+
+
+colors = {
+    "R": 0xFF5555AA,
+    "G": 0xFF76A371,
+    "B": 0xFFA07D4F
+}
+
+
+CollapsableFrame_style = {
+    "CollapsableFrame": {
+        "background_color": 0xFF343432,
+        "secondary_color": 0xFF343432,
+        "color": 0xFFAAAAAA,
+        "border_radius": 4.0,
+        "border_color": 0x0,
+        "border_width": 0,
+        "font_size": 14,
+        "padding": 0,
+    },
+    "HStack::header": {"margin": 5},
+    "CollapsableFrame:hovered": {"secondary_color": 0xFF3A3A3A},
+    "CollapsableFrame:pressed": {"secondary_color": 0xFF343432},
+}
+
+ScrollingFrame_style = {
+    "ScrollingFrame": {
+        "background_color": 0xFF343432,
+        "secondary_color": 0xFF343432,
+        "color": 0xFFAAAAAA,
+        "border_radius": 4.0,
+        "border_color": 0x0,
+        "border_width": 0,
+        "font_size": 14,
+        "padding": 0,
+    },
+    "HStack::header": {"margin": 5},
+    "ScrollingFrame:hovered": {"secondary_color": 0xFF3A3A3A},
+    "ScrollingFrame:pressed": {"secondary_color": 0xFF343432},
+}
+
 class Uav_raceExtension(omni.ext.IExt):
-    # ext_id is current extension id. It can be used with extension manager to query additional information, like where
-    # this extension is located on filesystem.
     def on_startup(self, ext_id):
-        print("[uav_race] uav_race startup")
-
-        self._count = 0
-
-        self._window = ui.Window("My Window", width=300, height=300)
-        with self._window.frame:
-            with ui.VStack():
-                label = ui.Label("")
-
-
-                def on_click():
-                    self._count += 1
-                    label.text = f"count: {self._count}"
-
-                def on_reset():
-                    self._count = 0
-                    label.text = "empty"
-
-                on_reset()
-
-                with ui.HStack():
-                    ui.Button("Add", clicked_fn=on_click)
-                    ui.Button("Reset", clicked_fn=on_reset)
+        self.create_vars()
+        self.build_ui()
 
     def on_shutdown(self):
-        print("[uav_race] uav_race shutdown")
+        pass
+
+    def create_vars(self):
+        self.perspective_camera_path = "/OmniverseKit_Persp"
+        self.follow_UAV_camera_path = "/manual_controller_CAM"
+        self.amount_users = 4
+        self.viewports = {}
+
+    def build_ui(self):
+        self.window = ui.Window("NavSim - UAV Race", width=200, height=200)
+        self.window.frame.set_style(Window_dark_style)
+
+        with self.window.frame:
+            with ui.VStack(style=VStack_A, height=0):
+                self.add_viewports_button = ui.Button("ADD VIEWPORT", clicked_fn=self.add_viewports)
+
+                # self.add_viewports_collapsable = ui.CollapsableFrame("Joysticks")
+                # self.add_viewports_collapsable.set_style(CollapsableFrame_style)
+                # with self.add_viewports_collapsable:
+                #     ui.Label("Joystick1")
+
+    def add_viewports(self):
+        amount_viewports = 0
+        for viewport in omni.kit.viewport.window.get_viewport_window_instances():
+            if not viewport.visible:
+                viewport.destroy()
+                continue
+
+            amount_viewports += 1
+        
+        viewport_width = ui.Workspace.get_main_window_width()/4
+        viewport_height = ui.Workspace.get_main_window_height()/3
+        render_resolution_scale = 0.5
+
+        viewport_window = ui.Window(title=f"User {amount_viewports}", width=viewport_width, height=viewport_height+20, 
+                                    raster_policy=ui.RasterPolicy.NEVER)
+        viewport_window.set_visibility_changed_fn(self.viewport_on_visibility_change)
+        with viewport_window.frame:
+            viewport_widget = ViewportWidget(resolution=(640, 360))
+
+        viewport_api = viewport_widget.viewport_api
+        viewport_api.camera_path = self.perspective_camera_path
+
+        # print(viewport_api.projection)
+        # print(viewport_api.transform)
+
+        viewport_window = omni.kit.viewport.window.ViewportWindow(name=f"User {amount_viewports}", 
+                                                                  width=viewport_width, height=viewport_height)
+        viewport_window.setPosition(viewport_width, 0)
+        viewport_window.viewport_api.set_active_camera(self.perspective_camera_path)
+        viewport_window.set_visibility_changed_fn(self.viewport_on_visibility_change)
+
+        viewport_api = viewport_window.viewport_api
+
+        # Performance settings
+        # viewport_window.viewport_api.get_render_settings().antialiasing_mode = 0
+        # viewport_window.viewport_api.get_render_settings().enable_shadows(False)
+        # viewport_window.viewport_api.get_render_settings().max_fps = 30
+        # viewport_api.resolution_scale = 0.5
+        viewport_api.resolution = (640, 360)
+        # viewport_window.viewport_api.enable_culling(True)
+        # viewport_window.viewport_api.get_render_settings().enable_bloom(False)
+        # viewport_window.viewport_api.get_render_settings().enable_ambient_occlusion(False)
+        # viewport_window.viewport_api.get_render_settings().enable_depth_of_field(False)
+
+        self.viewports[viewport_window.title] = viewport_window
+
+    def viewport_on_visibility_change(self, visible):
+        for viewport_window in omni.kit.viewport.window.get_viewport_window_instances():
+            if not viewport_window.visible:
+                viewport_window.destroy()
+                self.viewports.pop(viewport_window.title)
