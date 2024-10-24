84062cd9cf455ddff17fc1f6711e6155c8febff4
BlazquezVictor <Victor.Blazquez@uclm.es>
2024-10-24 11:50:36 +0200

    multi manual controller ext actualizacion

--- ov/extensions/multi_manual_controller/multi_manual_controller_python/extension.py
+++ ov/extensions/multi_manual_controller/multi_manual_controller_python/extension.py
@@ -1,43 +1,26 @@
 import omni.ext
 import omni.ui as ui
+from .controller import Controller
 
-
-# Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
-def some_public_function(x: int):
-    print("[multi_manual_controller] some_public_function was called with x: ", x)
-    return x ** x
-
-
-# Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
-# instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
-# on_shutdown() is called.
 class Multi_manual_controllerExtension(omni.ext.IExt):
-    # ext_id is current extension id. It can be used with extension manager to query additional information, like where
-    # this extension is located on filesystem.
     def on_startup(self, ext_id):
-        print("[multi_manual_controller] multi_manual_controller startup")
-
-        self._count = 0
-
-        self._window = ui.Window("My Window", width=300, height=300)
-        with self._window.frame:
-            with ui.VStack():
-                label = ui.Label("")
-
-
-                def on_click():
-                    self._count += 1
-                    label.text = f"count: {self._count}"
-
-                def on_reset():
-                    self._count = 0
-                    label.text = "empty"
-
-                on_reset()
-
-                with ui.HStack():
-                    ui.Button("Add", clicked_fn=on_click)
-                    ui.Button("Reset", clicked_fn=on_reset)
+        self.create_vars()
+        self.build_ui()
 
     def on_shutdown(self):
-        print("[multi_manual_controller] multi_manual_controller shutdown")
+        self.stop()
+
+    def create_vars(self):
+        self.controller = Controller()
+
+    def build_ui(self):
+        self.window = ui.Window("NavSim - Multi Manual Controller", width=600, height=600)
+        
+        with self.window.frame:
+                ui.Button(text="START", clicked_fn=self.start)
+
+    def start(self):
+        self.controller.start()
+
+    def stop(self):
+        self.controller.stop()
