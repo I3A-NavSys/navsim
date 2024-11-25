f7b0474b7a8f5848f96bb89b76d8adeb6a27c276
Rafael Casado <rafael.casado@uclm.es>
2024-11-25 11:15:03 +0100

    GridRouting
    versión inicial del sistema de navegación en malla

---
+++ ov/tmp/tmpRafa/planners/GridPlanner.py
@@ -0,0 +1,149 @@
+import numpy as np
+from collections import deque
+
+class GridPlanner:
+
+    def __init__(self, cell_side=100, slot_time=10, x_height=60, y_height=100):
+        self.cell_side = cell_side  # Tamaño de las celdas           (m)
+        self.slot_time = slot_time  # Duración de cada slot          (s)
+        self.x_height = x_height    # Altura del subnivel este/oeste (m)
+        self.y_height = y_height    # Altura del subnivel norte/sur  (m)
+        self.grid = {}              # Diccionario de celdas
+
+
+    def GetTakeOffNodes(self, posXY, time):
+        """
+        Dada la posición 2D del vertipuerto en el área, 
+        devuelve los dos nodos a los que podemos conectar al despegue
+        """
+        (i,j) = np.array(posXY) // self.cell_side
+        s = time // self.slot_time
+        if j % 2 == 0:
+            return [(i+1,j  ,'X',s+2), (i-1,j+1,'X',s+2)]
+        else:
+            return [(i+1,j+1,'X',s+2), (i-1,j  ,'X',s+2)]
+        
+
+    def GetLandingNodes(self, posXY):
+        """
+        Dada la posición 2D del vertipuerto en el área, 
+        devuelve los dos nodos desde los que podemos aterrizar
+        """
+        (i,j) = np.array(posXY) // self.cell_side
+        if j % 2 == 0:
+            return [(i+1,j+1,'X',None), (i-1,j  ,'X',None)]
+        else:
+            return [(i-1,j+1,'X',None), (i+1,j  ,'X',None)]
+
+
+    def GetNextNode(self, node):
+        """
+        Dado un nodo, devuelve el nodo siguiente en línea recta.
+        """
+        (i,j,L,s) = node
+        if L == 'X':
+            if j % 2 == 0:                  
+                return (i+1,j  ,'X',s+1)        # rumbo ESTE
+            else:                           
+                return (i-1,j  ,'X',s+1)        # rumbo OESTE
+        else: # L == 'Y'
+            if i % 2 == 0:                  
+                return (i  ,j+1,'Y',s+1)        # rumbo NORTE
+            else:                           
+                return (i  ,j-1,'Y',s+1)        # rumbo SUR
+
+
+    def GetCrossNode(self, node):
+        """
+        Dado un nodo, devuelve el nodo siguiente en cruce.
+        """
+        (i,j,L,s) = node
+        if L == 'X':
+            if j % 2 == 0:                  
+                if i % 2 == 0:              
+                    return (i+1,j-1,'Y',s+1)    # giro ESTE -> SUR
+                else:                       
+                    return (i+1,j  ,'Y',s+1)    # giro ESTE -> NORTE
+            else:                           
+                if i % 2 == 0:              
+                    return (i  ,j  ,'Y',s+1)    # giro OESTE -> NORTE
+                else:                       
+                    return (i  ,j-1,'Y',s+1)    # giro OESTE -> SUR
+        else: # L == 'Y'
+            if i % 2 == 0:                  
+                if j % 2 == 0:              
+                    return (i-1,j+1,'X',s+1)    # giro NORTE -> OESTE
+                else:                       
+                    return (i  ,j+1,'X',s+1)    # giro NORTE -> ESTE
+            else:                           
+                if j % 2 == 0:              
+                    return (i  ,j  ,'X',s+1)    # giro SUR -> ESTE
+                else:                       
+                    return (i-1,j  ,'X',s+1)    # giro SUR -> OESTE
+
+
+    def GetRoute(self, node1, node2):
+        """
+        Dados dos nodos, devuelve una ruta libre del primero al segundo,
+        partiendo en el slot especificado.
+        """
+        (i2,j2,L2,s2) = node2
+        # BFS
+        explored_nodes = []
+        routes = deque()
+        routes.append([node1])
+        while routes:
+            route = routes.popleft()
+            node = route[-1]
+            (i,j,L,s) = node
+            if (i,j,L) in explored_nodes:
+                continue
+            explored_nodes.append((i,j,L))
+            if node in self.grid:
+                continue
+            if (i,j,L) == (i2,j2,L2):
+                return route
+            routes.append( route + [self.GetNextNode(node)] )
+            routes.append( route + [self.GetCrossNode(node)]  )
+        return None
+
+    def RouteLength(self, route):
+        """
+        Dada una ruta, devuelve su longitud.
+        """
+        return len(route) - 1
+
+
+    def ReserveNodes(self, route):
+        """
+        Reserva los nodos que componen la ruta especificada.
+        :param route: ruta.
+        """
+        for idx in range(len(route) - 1):
+            node1 = route[idx]
+            node2 = route[idx + 1]
+            if node1[2] == node2[2]:
+                self.grid[node1] = 'STR'    # STRaight line
+            else:
+                self.grid[node1] = 'TRN'    # TuRN
+        self.grid[node2] = 'END'            # END
+
+
+    def ClearRoute(self, route):
+        """
+        Libera la ruta especificada.
+        :param route: Lista de nodos que forman la ruta.
+        """
+        for node in route:
+            if node in self.grid:
+                del self.grid[node]
+
+    def AreThereConflicts(self, route):
+        """
+        Comprueba si esta ruta presenta conflictos con rutas existentes.
+        :param route: Lista de nodos que forman la ruta.
+        """
+        for node in route:
+            if node in self.grid:
+                return True
+        return False
---
+++ ov/tmp/tmpRafa/planners/testGP.py
@@ -0,0 +1,62 @@
+import sys
+import os
+
+#borrar consola
+os.system('cls')
+
+# Añadir el directorio principal al sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+##############################################################################
+
+
+from planners.GridPlanner import GridPlanner
+# from planners.FlightPlan  import FlightPlan
+# from planners.Waypoint    import Waypoint
+
+
+gp = GridPlanner()
+
+TOpos = (320, 270)  # posición de despegue   (m)
+TOtime = 33         # tiempo de despegue     (s)
+Lpos  = (380, 735)  # posición de aterrizaje (m)
+
+TOnodes = gp.GetTakeOffNodes(TOpos,TOtime)
+Lnodes  = gp.GetLandingNodes(Lpos)
+
+route1 = gp.GetRoute(TOnodes[0], Lnodes[0])
+print(route1)
+print(gp.RouteLength(route1))
+print(gp.AreThereConflicts(route1))
+print()
+
+route2 = gp.GetRoute(TOnodes[0], Lnodes[1])
+print(route2)
+print(gp.RouteLength(route2))
+print(gp.AreThereConflicts(route2))
+print()
+
+route3 = gp.GetRoute(TOnodes[1], Lnodes[0])
+print(route3)
+print(gp.RouteLength(route3))
+print(gp.AreThereConflicts(route3))
+print()
+
+route4 = gp.GetRoute(TOnodes[1], Lnodes[1])
+print(route4)
+print(gp.RouteLength(route4))
+print(gp.AreThereConflicts(route4))
+print()
+
+
+
+
+
+
+# print("-------------------")
+# route = gp.GetRoute((2, 4, 'X', 5), (8, 4, 'X', -1))
+# route = gp.GetRoute((1, 4, 'X', 4), (8, 4, 'X', -1))
+# print(route)
+# print(gp.AreThereConflicts(route))
+# gp.ReserveNodes(route)
+
