53c7f64d7313b5b7bc45dde1ed3f047a0bff1ac3
BlazquezVictor <Victor.Blazquez@uclm.es>
2025-03-27 13:42:43 +0100

    entorno base para rl creado, entorno con rewards y termination creado, modelo de aerotaxi modificado, modulo de rewards personalizados creado

---
+++ isaac_lab/mdp/__init__.py
@@ -0,0 +1,5 @@
+"""This sub-module contains the functions that are specific to the cartpole environments."""
+
+from isaaclab.envs.mdp import *  # noqa: F401, F403
+
+from .rewards import *  # noqa: F401, F403
