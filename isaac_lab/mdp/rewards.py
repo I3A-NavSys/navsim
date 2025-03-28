74461fb17b4aa55b1bb9a31b53003ad9ea752ddb
BlazquezVictor <Victor.Blazquez@uclm.es>
2025-03-28 08:55:15 +0100

    rewards y terminations actualizadas

--- isaac_lab/mdp/rewards.py
+++ isaac_lab/mdp/rewards.py
@@ -1,32 +1,19 @@
 from __future__ import annotations
 
-from scipy.spatial.transform import Rotation as R
-import numpy as np
 import torch
 from typing import TYPE_CHECKING
 
 from isaaclab.assets import Articulation
 from isaaclab.managers import SceneEntityCfg
-from isaaclab.utils.math import wrap_to_pi
-import numpy as np
 
 if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedRLEnv
 
 
-def lin_vel_diff(env: ManagerBasedRLEnv, target: np.array, asset_cfg: SceneEntityCfg) -> torch.Tensor:
+def lin_vel_diff(env: ManagerBasedRLEnv, target: torch.Tensor, asset_cfg: SceneEntityCfg) -> torch.Tensor:
     """Penalize linear velocity deviation from a target value."""
     asset: Articulation = env.scene[asset_cfg.name]
-    lin_vel = np.array(asset.data.root_com_lin_vel_b.tolist())
-    
-    # compute the reward
-    return torch.tensor(np.linalg.norm(lin_vel - target), device=env.device)
+    lin_vel = asset.data.root_com_lin_vel_b
+    diff = (lin_vel - target).norm(dim=1, keepdim=True)
 
-def roll_pitch_termination(env: ManagerBasedRLEnv, asset_cfg: SceneEntityCfg) -> torch.Tensor:
-    """Terminate when the asset's roll or pitch exceeds a certain threshold."""
-    asset: Articulation = env.scene[asset_cfg.name]
-    quat = asset.data.root_com_quat_w.tolist()
-    quat = [quat[1], quat[2], quat[3], quat[0]]
-    roll, pitch, yaw = R.from_quat(quat).as_euler('xyz')
-    
-    return torch.logical_or(torch.abs(roll) > np.pi/2, torch.abs(pitch) > np.pi/2)
+    return diff
---
+++ isaac_lab/mdp/terminations.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from scipy.spatial.transform import Rotation as R
+import torch
+from typing import TYPE_CHECKING
+
+from isaaclab.assets import Articulation
+from isaaclab.managers import SceneEntityCfg
+
+if TYPE_CHECKING:
+    from isaaclab.envs import ManagerBasedRLEnv
+
+def roll_pitch_termination(env: ManagerBasedRLEnv, asset_cfg: SceneEntityCfg) -> torch.Tensor:
+    """Terminate when the asset's roll or pitch exceeds a certain threshold."""
+    asset: Articulation = env.scene[asset_cfg.name]
+    quat = asset.data.root_com_quat_w.tolist()
+    quat = [quat[1], quat[2], quat[3], quat[0]]
+    roll, pitch, yaw = R.from_quat(quat).as_euler('xyz')
+    torch.pi
+    
+    return torch.logical_or(torch.abs(roll) > torch.pi/2, torch.abs(pitch) > torch.pi/2)
+
+def lin_vel_z_termination(env: ManagerBasedRLEnv, asset_cfg: SceneEntityCfg) -> torch.Tensor:
+    """Terminate when the asset's linear velocity in the z direction exceeds a certain threshold."""
+    asset: Articulation = env.scene[asset_cfg.name]
+    lin_vel = asset.data.root_lin_vel_w
+    
+    return lin_vel[:, 2] <= -5.0
