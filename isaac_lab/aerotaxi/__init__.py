3b6d39c9bcca9600d517d23cf8980b2f3a5c5ac0
BlazquezVictor <Victor.Blazquez@uclm.es>
2025-04-04 14:21:55 +0200

    nuevo entorno que tiene en cuenta el comando de velocidad y rotación deseada. Además, se ha optimizado el código, aumentando el rendimiento en la simulación drásticamente

--- isaac_lab/aerotaxi/__init__.py
+++ isaac_lab/aerotaxi/__init__.py
@@ -7,8 +7,20 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.aerotaxi_env:UAVEnvCfg",
-        # "env_cfg_entry_point": f"{__name__}.model_based_aerotaxi_env:UAVEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.aerotaxi_command_env:UAVEnvCfg",
+        "rl_games_cfg_entry_point": f"{config.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{config.__name__}.rsl_rl_ppo_cfg:AerotaxiPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{config.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{config.__name__}:sb3_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-Hover-Aerotaxi-v1",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.model_based_aerotaxi_env:UAVEnvCfg",
         "rl_games_cfg_entry_point": f"{config.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{config.__name__}.rsl_rl_ppo_cfg:AerotaxiPPORunnerCfg",
         "skrl_cfg_entry_point": f"{config.__name__}:skrl_ppo_cfg.yaml",
