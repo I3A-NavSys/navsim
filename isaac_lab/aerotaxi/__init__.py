3c7d88d3254644adfd1a9e1d247ce99e3d851722
Aurelio Bermudez <aurelio.bermudez@uclm.es>
2025-05-05 13:56:33 +0200

    nuevo entorno para enseñar a volar en el eje Z

--- isaac_lab/aerotaxi/__init__.py
+++ isaac_lab/aerotaxi/__init__.py
@@ -40,3 +40,16 @@ gym.register(
         "sb3_cfg_entry_point": f"{config.__name__}:sb3_ppo_cfg.yaml",
     },
 )
+
+gym.register(
+    id="Isaac-Command-Aerotaxi-v1",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.isaac_command_aerotaxi_v1:UAVEnvCfg",
+        "rl_games_cfg_entry_point": f"{config.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{config.__name__}.rsl_rl_ppo_cfg:AerotaxiPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{config.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{config.__name__}:sb3_ppo_cfg.yaml",
+    },
+)
