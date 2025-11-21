812e663e5633b9c9957a770d8078b526fc81c47a
Chamfire <140488015+Chamfire@users.noreply.github.com>
2025-11-21 18:27:44 +0100

    arreglado train y play

--- isaac_lab/rl_algorithms/rsl_rl/train.py
+++ isaac_lab/rl_algorithms/rsl_rl/train.py
@@ -10,11 +10,14 @@
 
 import argparse
 import sys
+import os
 
 from isaaclab.app import AppLauncher
 
 # local imports
 import cli_args  # isort: skip
+root_navsim_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+sys.path.append(root_navsim_path)
 
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Train an RL agent with RSL-RL.")
@@ -137,7 +140,8 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
         agent_cfg.seed = seed
 
     # specify directory for logging experiments
-    log_root_path = os.path.join("logs", "rsl_rl", agent_cfg.experiment_name)
+    # log_root_path = os.path.join("logs", "rsl_rl", agent_cfg.experiment_name)
+    log_root_path = os.path.join(root_navsim_path, "isaac_lab", "logs", "rsl_rl", args_cli.task)
     log_root_path = os.path.abspath(log_root_path)
     print(f"[INFO] Logging experiment in directory: {log_root_path}")
     # specify directory for logging runs: {time-stamp}_{run_name}
