fa7b3ffee8d7ff9d4528edae12fb7292b1cc8971
BlazquezMVictor <blazquezm.victor@gmail.com>
2026-01-12 09:36:34 +0100

    ajuste del parámetro _landing_time  a int, pues sólo se indica el tiempo de llegada en el primer vertipuerto, no de todos

--- msgs/mission_manager_msgs.py
+++ msgs/mission_manager_msgs.py
@@ -10,7 +10,7 @@ from uspace.uav_operator.uav_operator import UAVOperator
 @dataclass
 class StopOver:
     _stop_list: list[str] = field(default_factory=list)
-    _landing_time: list[int] = field(default_factory=list)
+    _landing_time: int = field(default_factory=int)
     _stop_time: list[int] = field(default_factory=list)
 
 @dataclass
