14bf3b07bd902f6d6b5b0bf0e14e95d000ebe6b0
Chamfire <140488015+Chamfire@users.noreply.github.com>
2026-02-05 16:24:46 +0100

    pequeños bugs corregidos

--- isaac_lab/aerotaxi/__init__.py
+++ isaac_lab/aerotaxi/__init__.py
@@ -166,6 +166,6 @@ for i in range(100):
         disable_env_checker=True,
         kwargs={
             "env_cfg_entry_point": f"{env_command.__name__}.env_10.env:UAVEnvCfg",
-            "rsl_rl_cfg_entry_point": f"{env_command.__name__}.env_10.grid_hiperparametros.cfg_files.ppo_random_command5_{i+1}_cfg:Command6PPORunnerCfg",
+            "rsl_rl_cfg_entry_point": f"{env_command.__name__}.env_10.grid_hiperparametros.cfg_files.ppo_random_command6_{i+1}_cfg:Command6PPORunnerCfg",
         },
     )
