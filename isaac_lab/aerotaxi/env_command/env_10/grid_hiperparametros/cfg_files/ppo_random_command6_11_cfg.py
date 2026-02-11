b97c6ba2538a70d3c40b8c7bd6c8ee330c43085c
Chamfire <140488015+Chamfire@users.noreply.github.com>
2026-02-11 15:05:30 +0100

    arreglado bug de import en MyPPO

--- isaac_lab/aerotaxi/env_command/env_10/grid_hiperparametros/cfg_files/ppo_random_command6_11_cfg.py
+++ isaac_lab/aerotaxi/env_command/env_10/grid_hiperparametros/cfg_files/ppo_random_command6_11_cfg.py
@@ -22,7 +22,7 @@ class Command6PPORunnerCfg(RslRlOnPolicyRunnerCfg):
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.99,
+        entropy_coef=0.01,
         num_learning_epochs=4,
         num_mini_batches=8,
         learning_rate=0.0001,
--- isaac_lab/rl_v5_1_0/rsl_rl/MyOnPolicyRunner.py
+++ isaac_lab/rl_v5_1_0/rsl_rl/MyOnPolicyRunner.py
@@ -13,7 +13,7 @@ import warnings
 from collections import deque
 
 import rsl_rl
-from MyOnPolicyRunner import MyPPO
+from MyPPO import MyPPO
 from rsl_rl.env import VecEnv
 from rsl_rl.modules import ActorCritic, ActorCriticRecurrent, resolve_rnd_config, resolve_symmetry_config
 from rsl_rl.utils import resolve_obs_groups, store_code_state
