6bf0951da294dedd7f97fa19d2a23460280d3ad8
Chamfire <140488015+Chamfire@users.noreply.github.com>
2026-03-04 09:28:26 +0100

    entorno de seguimiento de punto guía configurado, a falta de revisar la reward

---
+++ isaac_lab/aerotaxi/env_command/env_13/grid_hiperparametros/cfg_files/run_all.ps1
@@ -0,0 +1,2 @@
+Write-Host "Ejecutando ppo_random_command9_11"
+& "C:/Users/Teresa/Desktop/RuralData/IsaacLab/IsaacLab/isaaclab.bat" -p "C:/Users/Teresa/Documents/GitHub/navsim/isaac_lab/rl_v5_1_0/rsl_rl/train.py" --task Isaac-Command9-Aerotaxi-RANDOM-11 --num_envs 4096 --headless
