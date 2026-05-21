2a9b8f2e02269980b9b45a16341896e3bf693cd3
Chamfire <140488015+Chamfire@users.noreply.github.com>
2026-05-21 09:34:50 +0200

    hover test final

---
+++ isaac_lab/aerotaxi/env_command/env_12/grid_hiperparametros/cfg_files/ppo_random_command8_7_cfg.py
@@ -0,0 +1,34 @@
+from isaaclab.utils import configclass
+from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, RslRlPpoAlgorithmCfg
+
+@configclass
+class Command8PPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 100 
+    max_iterations = 1001
+    save_interval = 100
+    experiment_name = "command8"
+    run_name = "ppo_random_command8_7"
+    resume = False
+    empirical_normalization = False
+    csv_path_metrics = "C:/Users/Teresa/Documents/GitHub/navsim/tmp/tmpTeresa/resultados_grid_command_v8"
+    callbacks = True
+    policy = RslRlPpoActorCriticCfg(
+        init_noise_std=0.5,
+        actor_hidden_dims=[256, 256],
+        critic_hidden_dims=[256, 256],
+        activation="tanh",
+    )
+    algorithm = RslRlPpoAlgorithmCfg(
+        value_loss_coef=2.0,
+        use_clipped_value_loss=True,
+        clip_param=0.1,
+        entropy_coef=0.05,
+        num_learning_epochs=12,
+        num_mini_batches=2,
+        learning_rate=0.003,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.97,
+        desired_kl=0.005,
+        max_grad_norm=1.0,
+    )
--- isaac_lab/aerotaxi/env_command/env_12/grid_hiperparametros/cfg_files/run_tests_3.ps1
+++ isaac_lab/aerotaxi/env_command/env_12/grid_hiperparametros/cfg_files/run_tests_3.ps1
@@ -1,5 +1,17 @@
 $ErrorActionPreference = 'Stop'
 
-Write-Host "Ejecutando Test final para Experimento 11..."
-& "C:/Users/Teresa/Desktop/RuralData/IsaacLab/IsaacLab/isaaclab.bat" -p "C:/Users/Teresa/Documents/GitHub/navsim/isaac_lab/rl_v5_1_0/rsl_rl/play.py" --task Isaac-Command8-Aerotaxi-RANDOM-11 --num_envs 256  --headless --checkpoint "C:/Users/Teresa/Documents/GitHub/navsim/tmp/tmpTeresa/resultados_grid_command_v8/best_model_ppo_random_command8_11_4096.pt"
+Write-Host "Ejecutando Test final para Experimento 9..."
+& "C:/Users/Teresa/Desktop/RuralData/IsaacLab/IsaacLab/isaaclab.bat" -p "C:/Users/Teresa/Documents/GitHub/navsim/isaac_lab/rl_v5_1_0/rsl_rl/play.py" --task Isaac-Command8-Aerotaxi-RANDOM-9 --num_envs 8196  --headless --checkpoint "C:/Users/Teresa/Documents/GitHub/navsim/tmp/tmpTeresa/resultados_grid_command_v8/best_model_ppo_random_command8_9_2048.pt"
+
+Write-Host "Ejecutando Test final para Experimento 6..."
+& "C:/Users/Teresa/Desktop/RuralData/IsaacLab/IsaacLab/isaaclab.bat" -p "C:/Users/Teresa/Documents/GitHub/navsim/isaac_lab/rl_v5_1_0/rsl_rl/play.py" --task Isaac-Command8-Aerotaxi-RANDOM-6 --num_envs 8196  --headless --checkpoint "C:/Users/Teresa/Documents/GitHub/navsim/tmp/tmpTeresa/resultados_grid_command_v8/best_model_ppo_random_command8_6_1024.pt"
+
+Write-Host "Ejecutando Test final para Experimento 1..."
+& "C:/Users/Teresa/Desktop/RuralData/IsaacLab/IsaacLab/isaaclab.bat" -p "C:/Users/Teresa/Documents/GitHub/navsim/isaac_lab/rl_v5_1_0/rsl_rl/play.py" --task Isaac-Command8-Aerotaxi-RANDOM-1 --num_envs 8196  --headless --checkpoint "C:/Users/Teresa/Documents/GitHub/navsim/tmp/tmpTeresa/resultados_grid_command_v8/best_model_ppo_random_command8_1_2048.pt"
+
+Write-Host "Ejecutando Test final para Experimento 3..."
+& "C:/Users/Teresa/Desktop/RuralData/IsaacLab/IsaacLab/isaaclab.bat" -p "C:/Users/Teresa/Documents/GitHub/navsim/isaac_lab/rl_v5_1_0/rsl_rl/play.py" --task Isaac-Command8-Aerotaxi-RANDOM-3 --num_envs 8196  --headless --checkpoint "C:/Users/Teresa/Documents/GitHub/navsim/tmp/tmpTeresa/resultados_grid_command_v8/best_model_ppo_random_command8_3_2048.pt"
+
+Write-Host "Ejecutando Test final para Experimento 26..."
+& "C:/Users/Teresa/Desktop/RuralData/IsaacLab/IsaacLab/isaaclab.bat" -p "C:/Users/Teresa/Documents/GitHub/navsim/isaac_lab/rl_v5_1_0/rsl_rl/play.py" --task Isaac-Command8-Aerotaxi-RANDOM-26 --num_envs 8196  --headless --checkpoint "C:/Users/Teresa/Documents/GitHub/navsim/tmp/tmpTeresa/resultados_grid_command_v8/best_model_ppo_random_command8_26_512.pt"
 
