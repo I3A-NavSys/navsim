7b08d4ce35b06622aae0b5a34e83bdce9f45c9a8
BlazquezVictor <Victor.Blazquez@uclm.es>
2026-09-09 14:51:23 +0200

    [terminado]: nueva escena de Madrid con Cesium

--- extensions/uav_control_bridge/uav_control_bridge_python/extension.py
+++ extensions/uav_control_bridge/uav_control_bridge_python/extension.py
@@ -26,183 +26,37 @@ class UAVControlBridge(omni.ext.IExt):
 
         uav_physics_indices = [
             self.uav_ids_to_physics_buffer["UAV_OPERATOR_0"]["UAV_0"],
-            self.uav_ids_to_physics_buffer["UAV_OPERATOR_0"]["UAV_1"]
         ]
 
         # UAV_0 FlightPlan
-        positions = np.array([
-            [-0.60193, -0.51592, 9.99345],
-            [-0.60193, -0.51592, 14.34305],
-            [75.11271, -43.3532, 19.27086],
-            [131.01348, 0, 19.27086],
-            [318.85296, 0, 19.27086],
-            [410.57898, -96.92693, -11.36428],
-            [436.30316, -157.16426, -48.94219],
-            [436.30316, -157.16426, -48.9422],
+        wps = 6
+        times = [0, 5, 40, 80, 140, 200]
+        pos = np.array([
+            [1042, 1028, 708],
+            [1042, 1028, 708],
+            [1098, 1305, 780],
+            [1169, 1681, 780],
+            [922, 2966, 810],
+            [1112, 5347, 845]
         ])
-        magnitudes = np.array([
-            np.linalg.norm(positions[1] - positions[0]),
-            np.linalg.norm(positions[2] - positions[1]),
-            np.linalg.norm(positions[3] - positions[2]),
-            np.linalg.norm(positions[4] - positions[3]),
-            np.linalg.norm(positions[5] - positions[4]),
-            np.linalg.norm(positions[6] - positions[5]),
-            np.linalg.norm(positions[7] - positions[6])
+        vel = np.array([
+            np.array([0,0,0]),
+            np.array([0,0,5]),
+            (pos[2] - pos[1]) / np.linalg.norm(pos[2] - pos[1]) * 10,
+            (pos[2] - pos[1]) / np.linalg.norm(pos[2] - pos[1]) * 20,
+            (pos[4] - pos[3]) / np.linalg.norm(pos[4] - pos[3]) * 20,
+            (pos[4] - pos[3]) / np.linalg.norm(pos[4] - pos[3]) * 20,
         ])
-        velocities = np.array([
-            [0, 0, 0],
-            (positions[2] - positions[1]) / magnitudes[1],
-            (positions[3] - positions[2]) / magnitudes[2],
-            (positions[4] - positions[3]) / magnitudes[3],
-            (positions[5] - positions[4]) / magnitudes[4],
-            (positions[6] - positions[5]) / magnitudes[5],
-            (positions[7] - positions[6]) / magnitudes[6],
-            [0, 0, 0]
-        ])
+    
+        fp = FlightPlan()
+        for i in range(wps):
+            fp.set_waypoint(
+                time=times[i],
+                pos=pos[i],
+                vel=vel[i]
+            )
 
-        velocities[1] *= np.array([3, 3, 2])
-        velocities[2] *= np.array([3, 3, 1])
-        velocities[3] += np.array([5, 5, 0])
-        velocities[4] *= np.array([3.5, 3, 1])
-        velocities[5] *= np.array([1, 2, 1])
-
-        wp_amount = len(positions)
-        times = np.zeros(wp_amount)
-        times[0] = 10
-        times[1] = 15
-        for i in range(2, wp_amount):
-            times[i] = times[i - 1] + magnitudes[i-1] / np.linalg.norm(velocities[i-1])
-
-        fp0 = FlightPlan()
-        fp0.set_waypoint(
-            label="wp",
-            time=times[0],
-            pos=positions[0],
-            vel=velocities[0],
-            heading=[velocities[1][0], velocities[1][1]]
-        )
-        fp0.set_waypoint(
-            label="wp_01",
-            time=times[1],
-            pos=positions[1],
-            vel=velocities[1],
-        )
-        fp0.set_waypoint(
-            label="wp_02",
-            time=times[2],
-            pos=positions[2],
-            vel=velocities[2],
-        )
-        fp0.set_waypoint(
-            label="wp_03",
-            time=times[3],
-            pos=positions[3],
-            vel=velocities[3],
-        )
-        fp0.set_waypoint(
-            label="wp_04",
-            time=times[4],
-            pos=positions[4],
-            vel=velocities[4],
-        )
-        fp0.set_waypoint(
-            label="wp_05",
-            time=times[5],
-            pos=positions[5],
-            vel=velocities[5],
-        )
-        fp0.set_waypoint(
-            label="wp_06",
-            time=times[6],
-            pos=positions[6],
-            vel=velocities[6],
-        )
-        fp0.set_waypoint(
-            label="wp_07",
-            time=times[7],
-            pos=positions[7],
-            vel=velocities[7],
-        )
-        fp0.connect_waypoints()
-
-        # UAV_1 FlightPlan
-        positions = np.array([
-            [436.30316, -157.16426, -48.94219],
-            [436.30316, -157.16426, -48.9422],
-            [394.76271, -142.86073, -11.36428],
-            [37.57808, -19.87221, 19.27086],
-            [-0.60193, -0.51592, 14.34305],
-            [-0.60193, -0.51592, 9.99345],
-        ])
-        magnitudes = np.array([
-            0,
-            np.linalg.norm(positions[2] - positions[1]),
-            np.linalg.norm(positions[3] - positions[2]),
-            np.linalg.norm(positions[4] - positions[3]),
-            np.linalg.norm(positions[5] - positions[4]),
-            0
-        ])
-        velocities = np.array([
-            [0, 0, 0],
-            (positions[2] - positions[1]) / magnitudes[1],
-            (positions[3] - positions[2]) / magnitudes[2],
-            (positions[4] - positions[3]) / magnitudes[3],
-            (positions[5] - positions[4]) / magnitudes[4],
-            [0, 0, 0],
-        ])
-
-        velocities[1] *= np.array([1, 1, 7])
-        velocities[2] *= np.array([5, 5, 1])
-        velocities[4] *= np.array([1, 1, 0.5])
-
-        times = np.zeros(6)
-        times[0] = 10
-        times[1] = 15
-        for i in range(2, 6):
-            times[i] = times[i - 1] + magnitudes[i-1] / np.linalg.norm(velocities[i-1])
-
-        fp1 = FlightPlan()
-        fp1.set_waypoint(
-            label="wp",
-            time=times[0],
-            pos=positions[0],
-            vel=velocities[0],
-            heading=[velocities[1][0], velocities[1][1]]
-        )
-        fp1.set_waypoint(
-            label="wp_01",
-            time=times[1],
-            pos=positions[1],
-            vel=velocities[1],
-        )
-        fp1.set_waypoint(
-            label="wp_02",
-            time=times[2],
-            pos=positions[2],
-            vel=velocities[2],
-        )
-        fp1.set_waypoint(
-            label="wp_05",
-            time=times[3],
-            pos=positions[3],
-            vel=velocities[3],
-        )
-        fp1.set_waypoint(
-            label="wp_06",
-            time=times[4],
-            pos=positions[4],
-            vel=velocities[4],
-        )
-        fp1.set_waypoint(
-            label="wp_07",
-            time=times[5],
-            pos=positions[5],
-            vel=velocities[5],
-        )
-        fp1.connect_waypoints()
-
-        flightplan_as_lists.append(fp0.to_lists())
-        flightplan_as_lists.append(fp1.to_lists())
+        flightplan_as_lists.append(fp.to_lists())
 
         times = [fp[0] for fp in flightplan_as_lists]
         positions = [fp[1] for fp in flightplan_as_lists]
